15676501434b5efac0652826f96a404bdf9b4bf9
JHVBhj <91900128+JHVBhj@users.noreply.github.com>
2022-01-10 18:18:10 +0100

    Add files via upload

---
+++ putto2.py
@@ -0,0 +1,40 @@
+def nyereményszorzó(a,b):
+    if a == 8 and b == 1:
+        return 10000
+    elif a == 8 and b == 0:
+        return 1000
+    elif a == 7 and b == 1:
+        return 150
+    elif a == 7 and b == 0:
+        return 50
+    elif a == 6 and b == 1:
+        return 24
+    elif a == 6 and b == 0:
+        return 8
+    elif a == 5 and b == 1:
+        return 4
+    elif a == 5 and b == 0:
+        return 2
+    elif a == 4 and b == 1:
+        return 1
+    else:
+        return 0
+
+assert nyereményszorzó(8,1) == 10000
+assert nyereményszorzó(8,0) == 1000
+assert nyereményszorzó(7,1) == 150
+assert nyereményszorzó(7,0) == 50
+assert nyereményszorzó(6,1) == 24
+assert nyereményszorzó(6,0) == 8
+assert nyereményszorzó(5,1) == 4
+assert nyereményszorzó(5,0) == 2
+assert nyereményszorzó(4,1) == 1
+assert nyereményszorzó(1,1) == 0
+
+feldolglista
+
+def splitter(lista):
+    for sor in range(len(lista):
+        
+    
+    
